407451441a4e4698ab83fbb3f2e222d456cc0056
Akhil Pillai <akhil.pillai@imaginea.com>
2018-08-20 22:03:30 +0530

    Added checks for amount sedn for purchasing a ticket

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,14 +9,19 @@ contract Lottery {
 
     event WinnerDeclared(address winner);
 
-    constructor(uint noOfTickets, uint costOfTicket) public payable {
+    modifier checkForExactValue(uint cost) {
+        require(msg.value == cost, "Exact amount should be send to buy a ticket");
+        _;
+    }
+
+    constructor(uint noOfTickets, uint costOfTicket) public payable checkForExactValue(costOfTicket) {
         totalTickets = noOfTickets;
         costPerTicket = costOfTicket;
 
         soldTickets.push(msg.sender);
     }
 
-    function buy() public payable {
+    function buy() public payable checkForExactValue(costPerTicket) {
         require(!ended, "Lottery has already ended");
         soldTickets.push(msg.sender);
         if (soldTickets.length == totalTickets) {
