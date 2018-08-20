ac5c4c805dd347773801d92952f114329ac63134
Akhil Pillai <akhil.pillai@imaginea.com>
2018-08-20 21:03:54 +0530

    Added test to determine if the first ticket is sold to the contract owner

--- test/lottery.js
+++ test/lottery.js
@@ -2,7 +2,15 @@ const Lottery = artifacts.require("../contracts/Lottery.sol");
 
 contract("Lottery", accounts => {
     let lottery;
-    before("initialize a lottery object to be used by the test cases", () => {
-        lottery = Lottery.new(5, web3.toWei("1"), { from: accounst[0], value: web3.toWei("1") });
+    const owner = accounts[0];
+
+    before("initialize a lottery object to be used by the test cases", async () => {
+        lottery = await Lottery.new(5, web3.toWei("1"), { from: owner, value: web3.toWei("1") });
+    });
+
+    it("first ticket should be purchased by the owner", () => {
+        return lottery.soldTickets.call(0).then(account => {
+            assert.equal(account, owner, "first ticket not sold to owner");
+        })
     })
 })
